8cec1655a75a3a219baecb9dbeb85e5d5f757f0f
Þór Tómasarson <Thor.Tomasarson@marel.com>
2020-01-25 20:05:20 +0000

    Moving files and stuff

---
+++ Scripts/SpectralReflectanceData/transform.m
@@ -0,0 +1,11 @@
+myFiles = dir('*.dpt'); %gets all wav files in struct
+
+for k = 1:length(myFiles)
+  fileName = myFiles(k).name;
+  fprintf(1, 'Now reading %s\n', fileName);
+  a = load(fileName);
+  a(:, 1) = 10 * 1e6 ./ (a(:, 1));
+  fileNameStuff = split(fileName, '.');
+  newFileName = string(fileNameStuff(1)) + ".csv";
+  csvwrite(newFileName, a);
+end
