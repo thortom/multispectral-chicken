ccd962100269f8bc26e35ea9f6ddaf1f996496a4
Þór Tómasarson <Thor.Tomasarson@marel.com>
2020-04-26 11:41:44 +0000

    Same CrossValidation on SVM for the 10 layer films

--- mypackage/Models/ClassicalLearners.py
+++ mypackage/Models/ClassicalLearners.py
@@ -2,9 +2,69 @@ import numpy as np
 import tensorflow as tf
 import mypackage
 import IPython
+import joblib
 import matplotlib.pyplot as plt
+from sklearn import svm
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score, precision_score, recall_score
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+
+timer = mypackage.utils.Timer()
+
+class SVM:
+    def __init__(self, X_train, Y_train, saved_mode_name="latest_svm_model.sav"):
+        self.saved_mode_name  = saved_mode_name
+        self.X_train          = X_train
+        self.Y_train          = Y_train
+        self.svc_classifier   = None
+        
+    def train(self):
+        train = mypackage.StackTransform(self.X_train, self.Y_train)
+
+        self.svc_classifier = svm.SVC(C=1, kernel='rbf', decision_function_shape='ovr')
+        timer.start()
+        self.svc_classifier.fit(train.X_stack(), train.Y_stack().ravel())
+        timer.stop()
+
+        self.save_model()
+        
+    def save_model(self):
+        # save the model to disk
+        joblib.dump(self.svc_classifier, self.saved_mode_name)
+        
+    def load_model(self):
+        # load the model from disk
+        self.svc_classifier = joblib.load(self.saved_mode_name)
+    
+    def predict(self, X, Y=None, plot=True):
+        test = mypackage.StackTransform(X, Y)
+        
+        timer.start()
+        Y_hat_stacked = self.svc_classifier.predict(test.X_stack())
+        timer.stop()
+
+        Y_hat = test.Unstack(Y_hat_stacked, k=1)
+        
+        if Y is not None:
+            classification = classification_report(test.Y_stack(), Y_hat_stacked) # .flatten()
+            print(classification)
+
+            if plot:
+                selected = np.random.choice(len(Y))
+                
+                plt.figure(figsize=(9, 5))
+                plt.subplot(121)
+                plt.title("True Classes")
+                plt.imshow(np.squeeze(Y[selected]))
+                plt.axis('off')
+                plt.subplot(122)
+                plt.title("SVM Classification")
+                img = plt.imshow(np.squeeze(Y_hat[selected]))
+                mypackage.Dataset._Dataset__add_legend_to_image(Y_hat[selected], img)
+                plt.axis('off');
+
+        return Y_hat
+    
 
 def logistic_regression(X_train, Y_train, X_test, Y_test, C=1e5, plot=True, metrics=True, max_iter=10000):
     train = mypackage.StackTransform(X_train, Y_train)
--- mypackage/__init__.py
+++ mypackage/__init__.py
@@ -6,7 +6,7 @@ from .DataManipulation.data import StackTransform
 from .DataManipulation.fake_data import FakeDataset
 from .Models import standard_unet
 from .Models.ClassicalLearners import logistic_regression
-from .Models.ClassicalLearners import logistic_regression
+from .Models.ClassicalLearners import SVM
 from .Models import PartialLeastSquares as PLS
 from .Models import MyUnet
 from .Models.HybridSN import HybridSN
